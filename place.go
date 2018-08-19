a3f06e1799837be6c7ffa75c8ccdb81d36959912
tomocy <tomocy.dev@gmail.com>
2018-08-19 13:23:39 +0900

    add var for api key

--- place.go
+++ place.go
@@ -1,5 +1,13 @@
 package meander
 
+import "os"
+
+var APIKey string
+
+func init() {
+	APIKey = os.Getenv("GOOGLE_PLACES_API_KEY")
+}
+
 type place struct {
 	Name            string         `json:"name"`
 	Icon            string         `json:"icon"`
