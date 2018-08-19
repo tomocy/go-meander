975093a329ac55d3a3e70406c1cea2deb0b864af
tomocy <tomocy.dev@gmail.com>
2018-08-19 16:55:00 +0900

    remove

--- public.go
+++ public.go
@@ -1,11 +1,9 @@
 package meander
 
-// Facade is an interface which display data human friendly
 type Facade interface {
 	Public() interface{}
 }
 
-// Public returns the result of Facade.Public() if the passed value is Facade or returns it
 func Public(o interface{}) interface{} {
 	if f, ok := o.(Facade); ok {
 		return f.Public()
