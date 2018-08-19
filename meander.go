f7a85097528acea39da9285189b55515f3ecebdd
tomocy <tomocy.dev@gmail.com>
2018-08-19 12:35:19 +0900

    add type for a jouney

---
+++ meander.go
@@ -0,0 +1,44 @@
+package meander
+
+type j struct {
+	Name       string
+	PlaceTypes []string
+}
+
+// Journeys is a slice of an arrary of interfaces which represents several journeys
+var Journeys = []interface{}{
+	&j{
+		Name: "Romantic",
+		PlaceTypes: []string{
+			"park", "bar", "movie_theater", "restaurant",
+			"florist", "taxi_stand",
+		},
+	},
+	&j{
+		Name: "Shopping",
+		PlaceTypes: []string{
+			"department_store", "cafe", "clothing_store", "jewelry_store",
+			"shoe_store",
+		},
+	},
+	&j{
+		Name: "Night Life",
+		PlaceTypes: []string{
+			"bar", "casino", "food", "bar",
+			"night_club", "bar", "bar", "hospital",
+		},
+	},
+	&j{
+		Name: "Culture",
+		PlaceTypes: []string{
+			"museum", "cafe", "cemetery", "library",
+			"art_gallery",
+		},
+	},
+	&j{
+		Name: "Relax",
+		PlaceTypes: []string{
+			"hair_care", "beauty_salon", "cafe", "spa",
+		},
+	},
+}
